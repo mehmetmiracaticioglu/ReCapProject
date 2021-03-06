993027d5bb83920620c5619036b4b5cc21acc062
mehmetmiracaticioglu <mehmetmiracaticioglu@gmail.com>
2021-03-06 18:45:15 +0300

    7.gün ödevi tamamlandı

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarManager carManager = new ICarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll()) 
+            {
+                Console.WriteLine(car.Description);
+            }
+
+
+        }
+
+       
+        
+        
+     
+    }
+}
